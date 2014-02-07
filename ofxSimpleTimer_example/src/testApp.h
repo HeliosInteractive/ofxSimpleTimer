8f9caa4b0db9e9d44c432ee491cf2af6481ef353
danthemellowman <dan@makeitdoathing.com>
2014-02-07 14:22:51 -0500

    adding ofEvent callbacks for TIMER_PAUSED and TIMER_STARTED

--- ofxSimpleTimer_example/src/testApp.h
+++ ofxSimpleTimer_example/src/testApp.h
@@ -4,21 +4,27 @@
 #include "ofxSimpleTimer.h"
 
 class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-        void mousePressed(int x, int y, int button);
     
-        ofxSimpleTimer timer1 ;
-        ofxSimpleTimer timer2 ;
+public:
+    void setup();
+    void update();
+    void draw();
     
-        ofColor color1 ;
-        ofColor color2 ;
+    void mousePressed(int x, int y, int button);
     
-        void timer1CompleteHandler( int &args ) ;
-        void timer2CompleteHandler( int &args ) ;
-		
+    ofxSimpleTimer timer1 ;
+    ofxSimpleTimer timer2 ;
+    
+    ofColor color1 ;
+    ofColor color2 ;
+    
+    void timer1CompleteHandler( int &args ) ;
+    void timer2CompleteHandler( int &args ) ;
+    
+    void timer1PauseHandler( int &args ) ;
+    void timer2PauseHandler( int &args ) ;
+    
+    void timer1StartedHandler( int &args ) ;
+    void timer2StartedHandler( int &args ) ;
+    
 };
